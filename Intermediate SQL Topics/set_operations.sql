0b30f1529d1a573ea405c7412089689f67727eda
Gleizits <gleizits@protonmail.com>
2026-05-27 17:32:55 -0500

    SQL Topic: Unions, Intersections & Exceptions

---
+++ Intermediate SQL Topics/set_operations.sql
@@ -0,0 +1,7 @@
+SELECT column, another_column
+   FROM mytable
+UNION / UNION ALL / INTERSECT / EXCEPT
+SELECT other_column, yet_another_column
+   FROM another_table
+ORDER BY column DESC
+LIMIT n;
