db928f97bd11a91faf8fd41d33120f4bba12eb63
Gleizits <gleizits@protonmail.com>
2026-05-27 16:35:40 -0500

    SQL Lesson 7: OUTER JOINs

---
+++ Lesson_7.sql
@@ -0,0 +1,11 @@
+-- 1) Find the list of all buildings that have employees
+
+SELECT DISTINCT building FROM employees;
+
+-- 2) Find the list of all buildings and their capacity
+
+SELECT * FROM buildings;
+
+-- 3) List all buildings and the distinct employee roles in each building (including empty buildings)
+
+SELECT DISTINCT role, building_name FROM buildings LEFT JOIN employees ON buildings.building_name = employees.building;
